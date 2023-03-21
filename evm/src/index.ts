52dcd50ccd2418da70302eee4587133016ec17ee
Nick <nick@enshrine.ai>
2023-03-21 15:56:42 +0000

    evm: export factories & types

---
+++ evm/src/index.ts
@@ -0,0 +1,5 @@
+export { AttestationToken__factory as AttestationTokenFactory } from '../typechain-types/factories/AttestationToken__factory';
+export { AttestationToken } from '../typechain-types/AttestationToken';
+
+export { Lockbox__factory as LockboxFactory } from '../typechain-types/factories/Lockbox__factory';
+export { Lockbox } from '../typechain-types/Lockbox';
