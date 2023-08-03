776879a6d517cb59ad1931518bec8017d47303ae
Nick Hynes <nick@enshrine.ai>
2023-08-03 15:20:52 -0400

    website: add sponsors

--- website/.vitepress/theme/index.ts
+++ website/.vitepress/theme/index.ts
@@ -1,16 +1,17 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import Theme from 'vitepress/theme'
-import './style.css'
+import { h } from 'vue';
+import Theme from 'vitepress/theme';
+
+import './style.css';
+import SponsorsBox from './SponsorsBox.vue';
 
 export default {
   extends: Theme,
   Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+      'home-features-after': () => h(SponsorsBox),
+    });
   },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
-}
+  enhanceApp({ app, router, siteData }) {},
+};
