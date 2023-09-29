e9977eb7cb936de887d16d8de7858d71794d57a9
Nick Hynes <nick@enshrine.ai>
2023-09-29 16:20:30 +0000

    evm: infer TaskHubs on Emerald and Sapphire mainnets

--- evm/src/tasks/v1/hub/TaskHubNotifier.sol
+++ evm/src/tasks/v1/hub/TaskHubNotifier.sol
@@ -49,10 +49,10 @@ contract TaskHubNotifier is BaseTaskHubNotifier {
         uint256 ch = block.chainid;
         // Emerald
         if (ch == 0xa515) return 0x37Dfd1a00116d59a08B97D19F95f1c2a435fF5Df;
-        // if (ch == 0xa516) return 0x;
+        if (ch == 0xa516) return 0xd0dD1dFE79bB4Ad64d727Ee99F51cb968e949bf4;
         // Sapphire
         if (ch == 0x5aff) return 0x63c13Ee9BecC8f35dC034F026Ae8d18A4D1E7f0E;
-        // if (ch == 0x5afe) return 0x;
+        if (ch == 0x5afe) return 0x4F8f246b1aaa99810844Be20DbdD4F5B36740Ca4;
         // Local
         if (ch == 1337 || ch == 31337 || ch == 0x5afd) return address(new TaskHub());
         return address(0);
