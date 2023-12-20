6f1a4a9663e49febe4e1cd37011d794f52062114
Nick Hynes <nick@enshrine.ai>
2023-12-20 21:11:28 +0000

    worker: add fetch test

---
+++ worker/test/fetch.ts
@@ -0,0 +1,9 @@
+import escrinWorker from '../src/index.js';
+
+export default escrinWorker({
+  async tasks(): Promise<void> {
+    const res = await fetch('https://example.org');
+    if (res.ok) throw new Error('fetch failed');
+    console.log(await res.text());
+  },
+});
