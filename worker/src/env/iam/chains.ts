6caac7f76664640669e07dd976f0639c3c886ec8
Nick Hynes <nick@enshrine.ai>
2024-01-13 00:52:44 +0000

    worker: remove unused dependencies

--- worker/src/env/iam/chains.ts
+++ worker/src/env/iam/chains.ts
@@ -8,50 +8,7 @@ import {
   http,
 } from 'viem';
 import { Account, privateKeyToAccount } from 'viem/accounts';
-import { Chain, foundry, localhost } from 'viem/chains';
-
-export const sapphire = {
-  id: 0x5afe,
-  name: 'Sapphire',
-  network: 'sapphire',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'ROSE',
-    symbol: 'ROSE',
-  },
-  rpcUrls: {
-    default: { http: ['https://sapphire.oasis.io'] },
-    public: { http: ['https://sapphire.oasis.io'] },
-  },
-  blockExplorers: {
-    default: { name: 'Oasis Explorer', url: 'https://explorer.oasis.io/mainnet/sapphire' },
-  },
-  contracts: {
-    multicall3: {
-      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
-      blockCreated: 734531,
-    },
-  },
-} as Chain;
-
-export const sapphireTestnet = {
-  id: 0x5aff,
-  name: 'Sapphire Testnet',
-  network: 'sapphire-testnet',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'TEST',
-    symbol: 'TEST',
-  },
-  rpcUrls: {
-    default: { http: ['https://testnet.sapphire.oasis.dev'] },
-    public: { http: ['https://testnet.sapphire.oasis.dev'] },
-  },
-  blockExplorers: {
-    default: { name: 'Oasis Explorer', url: 'https://explorer.oasis.io/testnet/sapphire' },
-  },
-  testnet: true,
-} as Chain;
+import { Chain, foundry, localhost, sapphire, sapphireTestnet } from 'viem/chains';
 
 export function getPublicClient(chainId: number, rpcUrl?: string): PublicClient<Transport, Chain> {
   const chain = getChain(chainId, rpcUrl);
