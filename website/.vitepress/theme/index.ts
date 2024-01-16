f3df16a219ffb66bd367745109ab91d44a51607f
Nick Hynes <nick@enshrine.ai>
2024-01-16 18:08:53 +0000

    website: remove SponsorsBox

--- website/.vitepress/theme/index.ts
+++ website/.vitepress/theme/index.ts
@@ -3,14 +3,14 @@ import { h } from 'vue';
 import Theme from 'vitepress/theme';
 
 import './style.css';
-import SponsorsBox from './SponsorsBox.vue';
+// import SponsorsBox from './SponsorsBox.vue';
 
 export default {
   extends: Theme,
   Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-      'home-features-after': () => h(SponsorsBox),
+      // 'home-features-after': () => h(SponsorsBox),
     });
   },
   enhanceApp({ app, router, siteData }) {},
