a40555568224608f2ee6c9461203a624165d59b3
Nick Hynes <nick@escrin.org>
2024-02-20 15:32:04 +0800

    contracts: add UnstableSsssPermitter

---
+++ evm/contracts/identity/v1/permitters/SsssPermitter.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {IIdentityRegistry, IdentityId, Permitter} from "./Permitter.sol";
+
+contract UnstableSsssPermitter is Permitter {
+    /// The SSSS permitter does not respond directly to acquire/release identity requests.
+    error Unsupported();
+
+    event Configuration();
+
+    uint256 public immutable creationBlock;
+
+    constructor(address upstream) Permitter(upstream) {
+        creationBlock = block.number;
+    }
+
+    function setPolicy(IdentityId identity, bytes calldata /* config */) external {
+        (address registrant,) = IIdentityRegistry(_getIdentityRegistry()).getRegistrant(identity);
+        if (msg.sender != registrant) revert Unauthorized();
+        emit Configuration();
+    }
+
+    function _acquireIdentity(IdentityId, address, uint64, bytes calldata, bytes calldata)
+        internal
+        virtual
+        override
+        returns (uint64)
+    {
+        if (true) revert Unsupported();
+        return 0;
+    }
+
+    function _releaseIdentity(IdentityId, address, bytes calldata, bytes calldata)
+        internal
+        virtual
+        override
+    {
+        if (true) revert Unsupported();
+    }
+}
