bdaa2a5cd4f7cdbfc93a72724354f6075df8840b
Nick Hynes <nick@escrin.org>
2024-04-26 10:18:58 +0800

    evm: update SsssHub with VSS

--- evm/contracts/identity/v1/SsssHub.sol
+++ evm/contracts/identity/v1/SsssHub.sol
@@ -7,18 +7,27 @@ import {ExperimentalSsssPermitter} from "./permitters/SsssPermitter.sol";
 contract ExperimentalSsssHub is ExperimentalSsssPermitter {
     event SharesDealt();
 
+    mapping(IdentityId => mapping(string => uint256)) public versions;
+
     constructor(address upstream) ExperimentalSsssPermitter(upstream) {}
 
     function dealShares(
         IdentityId identity,
-        uint64, /* version */
+        string calldata secretName,
+        uint64 version,
         bytes calldata, /* pk */
         bytes32, /* nonce */
-        bytes[] calldata /* shares */
+        bytes[] calldata, /* shares */
+        bytes[] calldata, /* commitments */
+        bytes calldata /* userdata */
     ) external {
+        versions[identity][secretName] += 1;
+        require(version == versions[identity][secretName], "conflict");
         IIdentityRegistry.Permit memory permit =
             _getIdentityRegistry().readPermit(msg.sender, identity);
         if (permit.expiry <= block.timestamp) revert Unauthorized();
         emit SharesDealt();
     }
+
+    // TODO: add gassless deal shares
 }
