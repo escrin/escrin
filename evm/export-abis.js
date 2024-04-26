2c19b597a9ae5688151022c292b69d0117fca6de
Nick Hynes <nick@escrin.org>
2024-04-26 20:18:49 +0800

    evm: export abi jsons

--- evm/export-abis.js
+++ evm/export-abis.js
@@ -20,7 +20,10 @@ const srcdir = path.join(__dirname, 'contracts');
 const outdir = path.join(__dirname, 'out');
 const abidir = path.join(__dirname, 'abi');
 
-await fs.mkdir(outdir, { recursive: true });
+await fs.rm(abidir, { recursive: true, force: true });
+await Promise.all([fs.mkdir(outdir, { recursive: true }), fs.mkdir(abidir, { recursive: true })]);
+
+const copies = [];
 
 const abiStrs = [];
 for await (const solFile of findFiles(srcdir, '.sol')) {
@@ -29,12 +32,14 @@ for await (const solFile of findFiles(srcdir, '.sol')) {
   if (!stats.isDirectory()) continue;
   for await (const abiFile of findFiles(solOutDir, '.json')) {
     const abiName = path.basename(abiFile, '.json');
-    const { abi: abiJson } = JSON.parse(await fs.readFile(path.join(solOutDir, abiFile), 'utf-8'));
+    const abiPath = path.join(solOutDir, abiFile);
+    copies.push(fs.copyFile(abiPath, path.join(abidir, abiFile)));
+    const { abi: abiJson } = JSON.parse(await fs.readFile(abiPath, 'utf-8'));
     if (abiJson.length === 0) continue;
     abiStrs.push(`export const ${abiName} = ${JSON.stringify(abiJson, null, 2)} as const;`);
   }
 }
 
 const outfile = path.join(abidir, 'index.ts');
-await fs.unlink(outfile);
 await fs.writeFile(outfile, abiStrs.join('\n\n'));
+await Promise.all(copies);
