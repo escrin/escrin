b381d50f6d990536530ea0c81af276852f418a7b
Nick Hynes <nick@escrin.org>
2024-05-30 22:19:13 +0200

    evm: implement SsssPermitter acquire/release identity

--- evm/contracts/identity/v1/permitters/SsssPermitter.sol
+++ evm/contracts/identity/v1/permitters/SsssPermitter.sol
@@ -1,19 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
 import {IIdentityRegistry, IdentityId, Permitter} from "./Permitter.sol";
 
-contract ExperimentalSsssPermitter is Permitter {
-    /// The SSSS permitter does not respond directly to acquire/release identity requests.
-    error Unsupported(); // kKLK8g==
+contract SsssPermitter is Permitter, EIP712 {
+    /// The SSSS signature could not be verified against the expected permit.
+    error InvalidSsssSignature(); // xwXoVA== 0xc705e854
+    /// The SSSS signer/weight proof was not acceptable.
+    error InvalidProof(); // Cb3jOQ== 0x09bde339
+    /// Not enough SSSSs signed the permit.
+    error QuorumNotReached(); // qiamkw== 0xaa26a693
+    /// The nonce was already used.
+    error NonceUsed(); // H21a7w== 0x1f6d5aef
 
-    event PolicyChange();
-    event ApproverChange();
+    event PolicyChange(IdentityId identity);
+    event ApproverChange(IdentityId identity);
 
     mapping(IdentityId => bytes32) public policyHashes;
     mapping(IdentityId => bytes32) public approverRoots;
+    mapping(IdentityId => mapping(bytes32 => bool)) private burntNonces;
 
-    constructor(address upstream) Permitter(upstream) {}
+    constructor(address upstream) Permitter(upstream) EIP712("SsssPermitter", "1") {}
 
     modifier onlyRegistrant(IdentityId identity) {
         (address registrant,) = _getIdentityRegistry().getRegistrant(identity);
@@ -26,32 +37,104 @@ contract ExperimentalSsssPermitter is Permitter {
         onlyRegistrant(identity)
     {
         policyHashes[identity] = policyHash;
-        emit PolicyChange();
+        emit PolicyChange(identity);
     }
 
-    function setApproversRoot(IdentityId identity, bytes32 approversRoot)
+    function setApproversRoot(IdentityId identity, bytes32 signersRoot, uint256 threshold)
         external
         onlyRegistrant(identity)
     {
-        approverRoots[identity] = approversRoot;
-        emit ApproverChange();
+        approverRoots[identity] = _calculateApproversRoot(signersRoot, threshold);
+        emit ApproverChange(identity);
     }
 
-    function _acquireIdentity(IdentityId, address, uint64, bytes calldata, bytes calldata)
-        internal
-        virtual
-        override
-        returns (uint64)
-    {
-        if (true) revert Unsupported();
-        return 0;
+    function _acquireIdentity(
+        IdentityId identity,
+        address recipient,
+        uint64 duration,
+        bytes calldata context,
+        bytes calldata authorization
+    ) internal virtual override returns (uint64) {
+        _acqRelIdentity(identity, recipient, duration, true, context, authorization);
+        return duration;
     }
 
-    function _releaseIdentity(IdentityId, address, bytes calldata, bytes calldata)
+    function _releaseIdentity(
+        IdentityId identity,
+        address recipient,
+        bytes calldata context,
+        bytes calldata authorization
+    ) internal virtual override {
+        _acqRelIdentity(identity, recipient, 0, false, context, authorization);
+    }
+
+    function _acqRelIdentity(
+        IdentityId identity,
+        address recipient,
+        uint64 duration,
+        bool grant,
+        bytes calldata context,
+        bytes calldata authorization
+    ) private {
+        (uint256 threshold, bytes memory nonce, bytes memory pk, uint256 baseBlock) =
+            abi.decode(context, (uint256, bytes, bytes, uint256));
+
+        (bytes32[] memory proof, bool[] memory proofFlags, Signature[] memory signatures) =
+            abi.decode(authorization, (bytes32[], bool[], Signature[]));
+
+        if (signatures.length < threshold) revert QuorumNotReached();
+
+        bytes32 permitDigest = _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    keccak256(
+                        "SsssPermit(address registry,bytes32 identity,address recipient,bool grant,uint64 duration,bytes nonce,bytes pk,uint256 baseblock)"
+                    ),
+                    address(_getIdentityRegistry()),
+                    IdentityId.unwrap(identity),
+                    recipient,
+                    grant,
+                    duration,
+                    keccak256(nonce),
+                    keccak256(pk),
+                    baseBlock
+                )
+            )
+        );
+
+        bytes32[] memory leaves = new bytes32[](signatures.length);
+        for (uint256 i; i < signatures.length; i++) {
+            if (
+                ECDSA.recover(permitDigest, signatures[i].r, signatures[i].vs)
+                    != signatures[i].signer
+            ) {
+                revert InvalidSsssSignature();
+            }
+            leaves[i] = keccak256(bytes.concat(keccak256(abi.encode(signatures[i].signer))));
+        }
+
+        bytes32 signersRoot = MerkleProof.processMultiProof(proof, proofFlags, leaves);
+
+        if (_calculateApproversRoot(signersRoot, threshold) != approverRoots[identity]) {
+            revert InvalidProof();
+        }
+
+        bytes32 nonceHash = keccak256(nonce);
+        if (burntNonces[identity][nonceHash]) revert NonceUsed();
+        burntNonces[identity][nonceHash] = true;
+    }
+
+    struct Signature {
+        address signer;
+        bytes32 r;
+        bytes32 vs;
+    }
+
+    function _calculateApproversRoot(bytes32 signersRoot, uint256 threshold)
         internal
-        virtual
-        override
+        pure
+        returns (bytes32)
     {
-        if (true) revert Unsupported();
+        return keccak256(abi.encode(signersRoot, threshold));
     }
 }
